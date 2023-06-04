10a1e2ffb777e0382f0bcb125114183624fbe365
Ivan Reshetnikov <ordinarydev@protonmail.com>
2023-06-04 14:25:41 +0500

    Show list of boards on the main page

--- frontend/src/pages/boards/index.tsx
+++ frontend/src/pages/boards/index.tsx
@@ -1,14 +1,40 @@
-import { createResource } from 'solid-js'
+import { For, createResource } from 'solid-js'
 import type { Component } from 'solid-js'
+import { A } from '@solidjs/router'
 
-import { fetchBoards } from '../../api'
+import { Board, fetchBoards } from '../../api'
+import styles from './index.module.css'
 
 const BoardsPage: Component = () => {
     const [boards] = createResource(fetchBoards)
 
     return (
-        <div>
+        <div class={styles.Page}>
+            <Logo />
+            <For each={boards()} fallback={<div>Loading...</div>}>
+                {board => <BoardRecord
+                    code={board.code}
+                    name={board.name}
+                />}
+            </For>
         </div>
+    )
+}
+
+const Logo: Component = () => {
+    return (
+        <div class={styles.Logo}>
+            Microboard
+        </div>
+    )
+}
+
+const BoardRecord: Component<Board> = (props) => {
+    return (
+        <A href={`/boards/${props.code}`} class={styles.BoardRecord}>
+            <div class={styles.Code}>{ props.code }</div>
+            <div class={styles.Name}>{ props.name }</div>
+        </A>
     )
 }
 
